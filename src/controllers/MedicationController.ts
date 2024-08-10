4c30c9ed8d912a46ed5c70ba6d1a0be7939f0edc
Eissa Faheem <eissafaheem@gmail.com>
2024-08-10 12:30:16 +0530

    feat: insert medication bulk

--- src/controllers/MedicationController.ts
+++ src/controllers/MedicationController.ts
@@ -1,7 +1,9 @@
+import { Types } from "mongoose";
 import { BadRequestError } from "../classes/errors/badRequestError";
 import MedicationEndpoints from "../interfaces/endpoints/medicationEndpoints";
 import Medication from "../interfaces/schemaTypes/Medication";
 import MedicationModel from "../models/MedicationModel";
+import isMedication from "../utils/guards/isMedication";
 
 class MedicationController implements MedicationEndpoints {
     add(req: Request, res: Response) {}
@@ -16,6 +18,30 @@ class MedicationController implements MedicationEndpoints {
         const insertedMedication = await MedicationModel.create(medication);
 
         return insertedMedication;
+    }
+
+    async insertMedicationBulk(
+        medications: (Types.ObjectId | Medication)[]
+    ): Promise<Medication[]> {
+        for (let medication of medications) {
+            if (!isMedication(medication))
+                throw new BadRequestError("Invalid alternative");
+        }
+
+        const newMedications = await MedicationModel.insertMany(medications);
+
+        return newMedications.map((doc): Medication => {
+            const { _id, __v, medicationId, ...rest } =
+                doc.toObject() as Medication & {
+                    __v?: number;
+                    _id: Types.ObjectId;
+                };
+
+            return {
+                medicationId: _id,
+                ...rest,
+            };
+        });
     }
 }
 
