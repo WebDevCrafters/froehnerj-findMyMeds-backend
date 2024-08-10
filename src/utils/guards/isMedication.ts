453784445e5d9b45af63f52d2f932afd9ae57ea9
Eissa Faheem <eissafaheem@gmail.com>
2024-08-10 12:30:28 +0530

    feat: ismedication updated

--- src/utils/guards/isMedication.ts
+++ src/utils/guards/isMedication.ts
@@ -4,10 +4,7 @@ function isMedication(object: any): object is Medication {
     return (
         object &&
         typeof object === "object" &&
-        "_id" in object &&
         "name" in object &&
-        "dose" in object &&
-        "quantity" in object &&
         "pickUpDate" in object
     );
 }
