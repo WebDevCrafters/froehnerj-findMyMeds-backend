7514360635d2acd1f790b8ca8e7dbea071bff94e
Eissa Faheem <eissafaheem@gmail.com>
2024-08-25 12:50:42 +0530

    feat: mapper seperated

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -45,56 +45,39 @@ class SearchService {
 
         if (!searches) throw new NotFoundError();
 
-        const searchArrResult = searches.map((search) => {
-            let { _id, ...searchRes } = search.toObject();
-            searchRes.searchId = _id;
-
-            if (searchRes.medication) {
-                let { _id, ...medication } =
-                    searchRes.medication as Medication & {
-                        _id: Types.ObjectId;
-                    };
-
-                medication.medicationId = _id;
-                searchRes.medication = medication;
-            }
-
-            if (searchRes.medication.alternatives?.length) {
-                const formattedAlternatives =
-                    searchRes.medication.alternatives.map((ele) => {
-                        let { _id, ...alternative } = ele as Medication & {
-                            _id: Types.ObjectId;
-                        };
-                        alternative.medicationId = _id;
-                        return alternative;
-                    });
-                searchRes.medication.alternatives = formattedAlternatives;
-            }
-
-            /**
-                @todo: Send status also 
-            */
-
-            return searchRes;
-        });
+        const searchArrResult = searches.map((search) =>
+            this.makeSearchFromDoc(search)
+        );
 
         return searchArrResult;
     }
 
     async getSearchesInRadius(
+        userId: string | Types.ObjectId,
         requestUserCoordinates: number[],
         radiusMiles: number
     ) {
         const radiusRadians = radiusMiles / 3963.2;
 
         const nearbySearches = await SearchModel.find({
+            patient: { $ne: userId },
             status: SearchStatus.InProgress,
             location: {
                 $geoWithin: {
                     $centerSphere: [requestUserCoordinates, radiusRadians],
                 },
             },
-        });
+        })
+            .select("-__v")
+            .populate({
+                path: "medication",
+                select: "-__v",
+                populate: {
+                    path: "alternatives",
+                    model: "Medication",
+                    select: "-__v",
+                },
+            });
 
         if (!nearbySearches || nearbySearches.length === 0) {
             throw new NotFoundError("No nearby searches found");
@@ -109,14 +92,36 @@ class SearchService {
                 _id: Types.ObjectId;
             }
     ): Search {
-        const { _id, __v, ...rest } = searchDoc.toObject() as Search & {
-            __v?: number;
-            _id: Types.ObjectId;
-        };
-        return {
-            searchId: _id,
-            ...rest,
-        };
+        let { _id, ...searchRes } = searchDoc.toObject();
+        searchRes.searchId = _id;
+
+        if (searchRes.medication) {
+            let { _id, ...medication } = searchRes.medication as Medication & {
+                _id: Types.ObjectId;
+            };
+
+            medication.medicationId = _id;
+            searchRes.medication = medication;
+        }
+
+        if (searchRes.medication.alternatives?.length) {
+            const formattedAlternatives = searchRes.medication.alternatives.map(
+                (ele) => {
+                    let { _id, ...alternative } = ele as Medication & {
+                        _id: Types.ObjectId;
+                    };
+                    alternative.medicationId = _id;
+                    return alternative;
+                }
+            );
+            searchRes.medication.alternatives = formattedAlternatives;
+        }
+
+        /**
+                @todo: Send status also 
+            */
+
+        return searchRes;
     }
 }
 
