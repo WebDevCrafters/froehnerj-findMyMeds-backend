66f3cea80d80beaab7e31eefdf8189441cb0ae2e
Eissa Faheem <eissafaheem@gmail.com>
2024-08-28 22:13:21 +0530

    feat: port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
   console.log(`[server]: Server is running at http://localhost:${port}`);
