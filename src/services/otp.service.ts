1a5f3954c0955ef15a7f59cbe6edd3d5f37d6d2b
Eissa Faheem <eissafaheem@gmail.com>
2024-09-09 07:49:10 +0530

    feat: email html

--- src/services/otp.service.ts
+++ src/services/otp.service.ts
@@ -30,8 +30,8 @@ class OTPService {
         const mailOptions = {
             from: process.env.OTP_EMAIL,
             to: email,
-            subject: "Your OTP Code",
-            text: `Your OTP is ${otp}. It will expire in 10 minutes.`,
+            subject: "FindMyMeds Verification",
+            html: this.generateOtpMailBody(otp),
         };
 
         await transporter.sendMail(mailOptions);
@@ -50,6 +50,31 @@ class OTPService {
         }
 
         return false;
+    }
+
+    generateOtpMailBody(otp: string) {
+        return `
+          <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px;">
+            <h2 style="text-align: center; color: #f55e25;">FindMyMeds Verification</h2>
+            <p>Dear User,</p>
+            <p>We received a request to verify your email address for <strong>FindMyMeds</strong>. Please use the following One-Time Password (OTP) to complete your verification process:</p>
+            
+            <div style="text-align: center; padding: 10px; margin: 20px 0; font-size: 24px; font-weight: bold; color: #f55e25; border: 1px solid #f55e25; border-radius: 8px; display: inline-block;">
+              ${otp}
+            </div>
+            
+            <p>This OTP is valid for the next <strong>10 minutes</strong>. Please do not share it with anyone.</p>
+            
+            <p>If you did not request this verification, you can safely ignore this email.</p>
+            
+            <p>Thank you for choosing FindMyMeds!</p>
+            
+            <p style="margin-top: 40px;">Best regards,<br/>FindMyMeds Team</p>
+            
+            <hr style="margin-top: 40px; border: 0; border-top: 1px solid #e0e0e0;"/>
+            <p style="font-size: 12px; color: #999;">This is an automated email, please do not reply.</p>
+          </div>
+        `;
     }
 }
 
