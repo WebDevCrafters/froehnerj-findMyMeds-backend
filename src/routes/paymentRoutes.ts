528b170c489fcf91ef4fe3297fe47ef556a730a9
Eissa Faheem <eissafaheem@gmail.com>
2024-09-17 20:42:35 +0530

    feat: validate json handler not in webhook

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -5,12 +5,11 @@ import { validateTokenHandler } from "../middleware/validateTokenHandler";
 
 const paymentRouter = express.Router();
 
-paymentRouter.use(validateTokenHandler);
-paymentRouter.route("/").post(asyncHandler(paymentController.addPayment));
-paymentRouter.route("/").put(asyncHandler(paymentController.updatePayment));
-paymentRouter.route("/all").get(asyncHandler(paymentController.getAllPayments));
-paymentRouter.route("/").get(asyncHandler(paymentController.getActivePayment));
-paymentRouter.route("/stripe").post(asyncHandler(paymentController.stripeSession));
+paymentRouter.route("/").post(validateTokenHandler, asyncHandler(paymentController.addPayment));
+paymentRouter.route("/").put(validateTokenHandler, asyncHandler(paymentController.updatePayment));
+paymentRouter.route("/all").get(validateTokenHandler, asyncHandler(paymentController.getAllPayments));
+paymentRouter.route("/").get(validateTokenHandler, asyncHandler(paymentController.getActivePayment));
+paymentRouter.route("/stripe").post(validateTokenHandler, asyncHandler(paymentController.stripeSession));
 paymentRouter.route("/webhook").post(asyncHandler(paymentController.handleWebhook));
 
 export default paymentRouter;
